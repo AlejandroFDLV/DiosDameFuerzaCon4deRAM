871a22f418f8fe7b79c531bb04200b751b6fce51
AlejandroFDLV <alejandro.ucla@outlook.com>
2024-12-23 20:46:39 -0400

    OIIAIOOIIIAI

--- DiosDameFuerzaCon4deRAM/Program.cs
+++ DiosDameFuerzaCon4deRAM/Program.cs
@@ -11,6 +11,8 @@ namespace DiosDameFuerzaCon4deRAM
             Console.Write("Papu, papu despierta, ");
             // Al compilar, el mensaje de "Console.Write" se verá en la misma línea que el siguiente mensaje "Console.WriteLine"
             Console.WriteLine("tienes que hornear unos momazos");
+            Console.WriteLine("O II A I OO III A I");
+            //AA
         }
     }
 }
