7abb526511a1b68ed13dc5b29172af4afa9862e9
AlejandroFDLV <alejandro.ucla@outlook.com>
2024-12-23 20:36:16 -0400

    Agregar archivos de proyecto.

---
+++ DiosDameFuerzaCon4deRAM/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DiosDameFuerzaCon4deRAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //--23/12/24--
+            Console.WriteLine("AAAAAAA");
+            Console.WriteLine("WAZA");
+            Console.Write("Papu, papu despierta, ");
+            // Al compilar, el mensaje de "Console.Write" se verá en la misma línea que el siguiente mensaje "Console.WriteLine"
+            Console.WriteLine("tienes que hornear unos momazos");
+        }
+    }
+}
